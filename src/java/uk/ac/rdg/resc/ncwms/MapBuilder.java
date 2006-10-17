7649cdff2ec87946c9add2058a0d533d1c595775
jdb <jdb@f020452a-5e1f-0410-8fa4-ae2b6fb27d5e>
2006-10-17 16:37:33 +0000

    Removed R-tree code and worked towards efficient lat-lon to x-y mapping
    git-svn-id: https://ncwms.svn.sourceforge.net/svnroot/ncwms/trunk@17 f020452a-5e1f-0410-8fa4-ae2b6fb27d5e

---
+++ src/java/uk/ac/rdg/resc/ncwms/MapBuilder.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2006 The University of Reading
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University of Reading, nor the names of the
+ *    authors or contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package uk.ac.rdg.resc.ncwms;
+
+import ucar.nc2.dataset.NetcdfDataset;
+import uk.ac.rdg.resc.ncwms.dataprovider.DataLayer;
+import uk.ac.rdg.resc.ncwms.proj.RequestCRS;
+
+/**
+ * Class that builds a map image from a {@link GetMap} request.  A new
+ * MapBuilder is created with every request for thread safety.
+ *
+ * @author Jon Blower
+ * $Revision$
+ * $Date$
+ * $Log$
+ */
+public class MapBuilder
+{
+    private RequestCRS crs;
+    private DataLayer dl;
+      
+    /**
+     * Creates a new instance of MapBuilder
+     * @param crs The {@link RequestCRS} that describes the map area and projection
+     * @param dl The {@link DataLayer} from which the data will be read
+     */
+    public MapBuilder(RequestCRS crs, DataLayer dl)
+    {
+        this.crs = crs;
+        this.dl = dl;
+    }
+    
+    public static void main(String[] args) throws Exception
+    {
+        NetcdfDataset nc = NetcdfDataset.openDataset("C:\\data\\FOAM_20050422.0.nc");
+        NetcdfDataset nc2 = NetcdfDataset.openDataset("C:\\data\\FOAM_20050422.0.nc");
+        if (nc == nc2)
+        {
+            System.out.println("Identical");
+        }
+        nc.close();
+        nc2.close();
+    }
+    
+}
