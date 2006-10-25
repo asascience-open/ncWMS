58d2b4932d9fad368768504f87911bcd87c592c8
jdb <jdb@f020452a-5e1f-0410-8fa4-ae2b6fb27d5e>
2006-10-25 16:40:42 +0000

    Testing performance of data extraction: comparing Jython with Java
    git-svn-id: https://ncwms.svn.sourceforge.net/svnroot/ncwms/trunk@31 f020452a-5e1f-0410-8fa4-ae2b6fb27d5e

---
+++ src/java/uk/ac/rdg/resc/ncwms/datareader/DataReader.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright (c) 2006 The University of Reading
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University of Reading, nor the names of the
+ *    authors or contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package uk.ac.rdg.resc.ncwms.datareader;
+
+import java.util.Arrays;
+import ucar.ma2.Array;
+import ucar.ma2.Range;
+import ucar.nc2.dataset.CoordinateAxis1D;
+import ucar.nc2.dataset.NetcdfDataset;
+import ucar.nc2.dataset.grid.GeoGrid;
+import ucar.nc2.dataset.grid.GridCoordSys;
+import ucar.nc2.dataset.grid.GridDataset;
+
+/**
+ * Provides static methods for reading data and returning as float arrays.
+ * Called from nj22dataset.py
+ *
+ * @author Jon Blower
+ * $Revision$
+ * $Date$
+ * $Log$
+ */
+public class DataReader
+{
+    
+    public static float[] read(String location, String varID,
+        float fillValue, float[] lonValues, float[] latValues)
+        throws Exception
+    {
+        NetcdfDataset nc = null;
+        try
+        {
+            nc = NetcdfDataset.openDataset(location);
+            GridDataset gd = new GridDataset(nc);
+            GeoGrid geogrid = gd.findGridByName(varID);
+            if (geogrid == null)
+            {
+                nc.close();
+                return null;
+            }
+            GridCoordSys coordSys = geogrid.getCoordinateSystem();
+            if (!coordSys.isLatLon())
+            {
+                throw new Exception("Can only read data from lat-lon coordinate systems");
+            }
+
+            CoordinateAxis1D xAxis = (CoordinateAxis1D)coordSys.getXHorizAxis();
+            CoordinateAxis1D yAxis = (CoordinateAxis1D)coordSys.getYHorizAxis();
+            // TODO: handle t and z properly
+            Range tRange = new Range(0, 0);
+            Range zRange = new Range(0, 0);
+            // Find the range of x indices
+            int minX = Integer.MAX_VALUE;
+            int maxX = -Integer.MAX_VALUE;
+            int[] xIndices = new int[lonValues.length];
+            for (int i = 0; i < lonValues.length; i++)
+            {
+                xIndices[i] = xAxis.findCoordElement(lonValues[i]);
+                if (xIndices[i] >= 0)
+                {
+                    if (xIndices[i] < minX) minX = xIndices[i];
+                    if (xIndices[i] > maxX) maxX = xIndices[i];
+                }
+            }
+            Range xRange = new Range(minX, maxX);
+            // Create an array to hold the data
+            float[] picData = new float[lonValues.length * latValues.length];
+            Arrays.fill(picData, fillValue);
+            // Cycle through the latitude values, extracting a scanline of
+            // data each time from minX to maxX
+            for (int j = 0; j < latValues.length; j++)
+            {
+                int yIndex = yAxis.findCoordElement(latValues[j]);
+                if (yIndex >= 0)
+                {
+                    Range yRange = new Range(yIndex, yIndex);
+                    GeoGrid subset = geogrid.subset(tRange, zRange, yRange, xRange);
+                    Array array = subset.readYXData(0, 0).reduce();
+                    float[] rawData = (float[])array.getStorage();
+                    // Now copy the scanline's data to the picture array
+                    for (int i = 0; i < xIndices.length; i++)
+                    {
+                        if (xIndices[i] >= 0)
+                        {
+                            int picIndex = j * lonValues.length + i;
+                            picData[picIndex] = rawData[xIndices[i] - minX];
+                        }
+                    }
+                }
+            }
+
+            return picData;
+        }
+        finally
+        {
+            if (nc != null)
+            {
+                nc.close();
+            }
+        }
+    }
+    
+    public static void main(String[] args) throws Exception
+    {
+        String location = "C:\\data\\20061017-UKMO-L4UHfnd-GLOB-v01.nc";
+        String varID = "sst_foundation";
+        float fillValue = Float.NaN;
+        float[] lonValues = new float[256];
+        float[] latValues = new float[256];
+        for (int i = 0; i < 256; i++)
+        {
+            lonValues[i] = i * 90.0f / 256 - 90;
+            latValues[i] = i * 90.0f / 256;
+        }
+        float[] data = read(location, varID, Float.NaN, lonValues, latValues);
+    }
+}
--- web/WEB-INF/jython/nj22dataset.py
+++ web/WEB-INF/jython/nj22dataset.py
@@ -7,6 +7,8 @@ from ucar.ma2 import Range
 from java.lang import Integer
 from java.util import Arrays
 
+from uk.ac.rdg.resc.ncwms.datareader import DataReader
+
 import jarray
 
 class VariableMetadata: pass # simple class containing a variable's metadata
@@ -75,17 +77,16 @@ def readData(location, varID, grid, fillValue=1e20):
         returns an array of floating-point numbers representing the data,
             or None if the variable with id varID does not exist. """
 
-    nc = NetcdfDataset.openDataset(location)
-    gd = GridDataset(nc)
-    geogrid = gd.findGridByName(varID)
-    coordSys = geogrid.getCoordinateSystem()
-    if geogrid is None:
-        nc.close()
-        return None
+    #nc, geogrid, coordSys = openDataset(location, varID)
+    #if geogrid is None:
+    #    nc.close()
+    #    return None
     
     # TODO: relax these limitations:
     if not grid.isLatLon:
         raise "Can only read onto images in lat-lon projections"
+    return DataReader.read(location, varID, fillValue, grid.lonValues, grid.latValues)
+
     if not coordSys.isLatLon():
         raise "Can only read data from lat-lon coordinate systems"
 
@@ -99,7 +100,7 @@ def readData(location, varID, grid, fillValue=1e20):
     maxX = -Integer.MAX_VALUE
     xIndices = []
     for lon in grid.lonValues:
-        xIndex = xAxis.findCoordElement(lon) # TODO: findCoordElement() could be more efficient
+        xIndex = findCoordElement(xAxis, lon) # TODO: findCoordElement() could be more efficient
         xIndices.append(xIndex)
         if xIndex >= 0:
             if xIndex < minX : minX = xIndex
@@ -107,16 +108,15 @@ def readData(location, varID, grid, fillValue=1e20):
     xRange = Range(minX, maxX)
     # Create an array to hold the data
     # TODO: not sure this is the best way to do this
-    picData = jarray.zeros(grid.width * grid.height, 'f')
-    Arrays.fill(picData, fillValue)
+    picData = makePicData(grid, fillValue)
     # Cycle through the latitude values, extracting a scanline of
     # data each time from minX to maxX
     for j in xrange(len(grid.latValues)):
-        yIndex = yAxis.findCoordElement(grid.latValues[j])
+        yIndex = findCoordElement(yAxis, grid.latValues[j])
         if yIndex >= 0:
             yRange = Range(yIndex, yIndex)
-            subset = geogrid.subset(tRange, zRange, yRange, xRange)
-            array = subset.readYXData(0, 0).reduce()
+            subset = getSubset(geogrid, tRange, zRange, yRange, xRange)
+            array = readScanline(subset)
             rawData = array.getStorage()
             # Now copy the scanline's data to the picture array
             for i in xrange(len(xIndices)):
@@ -127,3 +127,27 @@ def readData(location, varID, grid, fillValue=1e20):
     # Close the source file and return the data
     nc.close()
     return picData
+
+# Provided as separate functions to aid profiling
+def openDataset(location, varID):
+    nc = NetcdfDataset.openDataset(location)
+    gd = GridDataset(nc)
+    geogrid = gd.findGridByName(varID)
+    if geogrid is not None:
+        coordSys = geogrid.getCoordinateSystem()
+    return (nc, geogrid, coordSys)
+
+def findCoordElement(axis, val):
+    return axis.findCoordElement(val)
+
+def makePicData(grid, fillValue):
+    picData = jarray.zeros(grid.width * grid.height, 'f')
+    Arrays.fill(picData, fillValue)
+    return picData
+
+def getSubset(geogrid, tRange, zRange, yRange, xRange):
+    return geogrid.subset(tRange, zRange, yRange, xRange)
+
+def readScanline(subset):
+    return subset.readYXData(0, 0).reduce()
+    
