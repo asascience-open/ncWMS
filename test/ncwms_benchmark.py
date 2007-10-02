056ee7b1661bff80ec126fd1df51c845256967e1
jonblower <jonblower@f020452a-5e1f-0410-8fa4-ae2b6fb27d5e>
2007-10-02 16:38:06 +0000

    Further mods to benchmarking code including ability to select layer at random
    git-svn-id: https://ncwms.svn.sourceforge.net/svnroot/ncwms/trunk@311 f020452a-5e1f-0410-8fa4-ae2b6fb27d5e

--- test/ncwms_benchmark.py
+++ test/ncwms_benchmark.py
@@ -3,26 +3,28 @@
 # are recorded by the server in the ncwms.benchmark log (which must be activated
 # in log4j.properties).
 
-# TODO: build a list of all possible requests, then sample randomly therefrom?
-
 from urllib2 import urlopen
 import random
 
 serverUrl = "http://localhost:8084/ncWMS/wms"
-layerName = "OSTIA/sst_foundation"
-sizes = [128, 256] # image widths/heights to use
+# Each of these layers uses a different algorithm to read the same data
+layerNames = ["OSTIA/sst_foundation", "OSTIA_BBOX/sst_foundation"] # PixelByPixelDataReader is slow!, "OSTIA_PBP/sst_foundation"]
+sizes = [64, 128, 256, 512] # image widths/heights to use
 
-zoom_levels = [1, 2, 3] # zoom_level 1 = whole earth, 2 = 1/4 earth
+zoom_levels = [1, 2, 3, 4, 5, 6] # zoom_level 1 = whole earth covered with two tiles
 maxNumRequests = (2 ** (zoom_levels[-1] - 1)) ** 2
 
 baseUrl  = '%s?REQUEST=GetMap&VERSION=1.3.0&STYLES=' % serverUrl
-baseUrl += '&FORMAT=image/png&CRS=CRS:84&EXCEPTIONS=XML&LAYERS=%s' % layerName
+baseUrl += '&FORMAT=image/png&CRS=CRS:84&EXCEPTIONS=XML'
 
-for i in xrange(20):
-    # Choose a random size and zoom level
+n = 2000
+for i in xrange(n):
+    
+    # Choose a random layer, size and zoom level
+    layerName = random.choice(layerNames)
     size = random.choice(sizes)
-    url2 = baseUrl + '&WIDTH=%d&HEIGHT=%d' % (size, size)
     z = random.choice(zoom_levels)
+    
     # Calculate the number of rows and columns of images tiles at this zoom level
     numrows = 2 ** (z - 1)
     numcols = numrows * 2 # So the tiles will be square in units of degrees
@@ -36,10 +38,12 @@ for i in xrange(20):
     endlat = startlat + side_length
     startlon = -180 + (col * side_length)
     endlon = startlon + side_length
-    url = url2 + '&BBOX=%f,%f,%f,%f' % (startlon, startlat, endlon, endlat)
-    print size, z, row, col, startlon, startlat, endlon, endlat
+    
+    url = baseUrl + '&LAYERS=%s&WIDTH=%d&HEIGHT=%d&BBOX=%f,%f,%f,%f' \
+        % (layerName, size, size, startlon, startlat, endlon, endlat)
+    print "%d/%d" % (i, n), layerName, size, z, row, col, startlon, startlat, endlon, endlat
     #print url
-    #urlopen(url).close() # We don't need to do anything with the data
+    urlopen(url).close() # We don't need to do anything with the data
     
 
 # This alternative code searches exhaustively through the zoom levels and image
